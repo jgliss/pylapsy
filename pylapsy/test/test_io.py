debda5f9a4ff1ca4692425a588afe6e8b1e012d3
jgliss <jonasgliss@gmail.com>
2020-02-15 21:20:11 +0100

    Update test_io.py

--- pylapsy/test/test_io.py
+++ pylapsy/test/test_io.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 #
-# This module is part of pylapsy. 
+# This module is part of pylapsy.
 # It is licensed under a GPL-3.0 license, for details see LICENSE file.
 #
 # Author: Jonas Gliß
-# Copyright (C) 2019 Jonas Gliss (jonasgliss@gmail.com) 
+# Copyright (C) 2019 Jonas Gliss (jonasgliss@gmail.com)
 # GitHub: jgliss
-# Email: jonasgliss@gmail.com 
+# Email: jonasgliss@gmail.com
 
 import pytest
 import os
@@ -19,20 +19,21 @@ def test_data_dir():
     except FileNotFoundError:
         ok = False
     assert ok
-    
+
 def test_get_test_image():
     f1 = os.path.basename(io.get_test_img(which=1))
     f2 = os.path.basename(io.get_test_img(which=2))
-    
+
     assert f1 == 'test_img1.jpg'
     assert f2 == 'test_img2.jpg'
-    
+
 def test_find_image_files():
     files = io.find_image_files(io.data_dir(), file_pattern='*.jpg')
-    names = [os.path.basename(x) for x in files]
+    names = sorted([os.path.basename(x) for x in files])
+    print(names)
     assert names == ['test_img1.jpg', 'test_img2.jpg']
-    
-    print(io.find_image_files(io.data_dir(), req_same_type=False))
-    
+
+    #print(io.find_image_files(io.data_dir(), req_same_type=False))
+
 if __name__ == '__main__':
     pytest.main(['test_io.py'])
